d75a4d7c2e0ce7a72d2e7233512b65f97c5bdf4a
Rakery Cheng <rcheng@csumb.edu>
2022-02-11 23:42:07 -0800

    Tutorial

---
+++ app/src/main/java/com/example/retrofitexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/retrofitexample/VolumesResponse.java
@@ -0,0 +1,27 @@
+package com.example.retrofitexample;
+
+public class VolumesResponse {
+    @SerializedName("kind")
+    @Expose
+    private String kind;
+
+    @SerializedName("items")
+    @Expose
+    List<Volume> items = null;
+
+    @SerializedName("totalItems")
+    @Expose
+    int totalItems;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public List<Volume> getItems() {
+        return items;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+}
